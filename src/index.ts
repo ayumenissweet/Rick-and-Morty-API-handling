5d179332c986d8e88a357eb3a864ad6ee5a48356
ayumenissweet <ayumishaste@gmail.com>
2026-06-04 18:27:01 +0100

    Expanded the program to do type narrowing and listen to many events

--- src/index.ts
+++ src/index.ts
@@ -26,25 +26,66 @@ type PayloadMap = EventMap<{
   };
 }>;
 type ReturnMap = EventMap<{
-  send: {};
-  receive: {};
-  "start-process": {};
-  login: {};
+  send: { token: string };
+  receive: { confirmed: boolean };
+  "start-process": { success: boolean };
+  login: { token: string };
 }>;
 function emitEvent<T extends EventTypes>(
   event: T,
   payload: PayloadMap[T],
 ): ReturnMap[T] {
-  return {} as ReturnMap;
+  //do your things here!
+  return {} as ReturnMap[T];
 }
 
-emitEvent("start-process", {
-  id: 111,
-  processName: "Google",
-  start_time: 10000,
+type AppEvent = {
+  [K in EventTypes]: {
+    type: K;
+    payload: PayloadMap[K];
+  };
+}[EventTypes];
+
+const output = emitEvent("start-process", {
+  id: 69,
+  processName: "cleanup",
+  start_time: 12,
   status: "Pending",
 });
+output.success; //typescript is smart enough to know the return output here
 
-function statusCheck(event: any): void /* this only display stuff */ {
-  //check all actions that are using that event and display them...
+//this function is used to narrow what is the type the user wants
+function statusCheck(event: AppEvent): void {
+  switch (event.type) {
+    //type script auto completes here
+    case "start-process":
+      console.log(
+        //typescript auto completes here too
+        `Process Started at ${event.payload.start_time}, status : ${event.payload.status}`,
+      );
+      break;
+    case "login":
+      console.log(`User logged in successly, username : ${event.payload.user}`);
+      break;
+    case "send":
+      console.log(
+        `Transaction Succesful : ${event.payload.amount}${event.payload.currency} sent from ${event.payload.src_country} to ${event.payload.dst_country}`,
+      );
+      break;
+    case "receive":
+      console.log(
+        `Transaction Succesful : ${event.payload.amount}${event.payload.currency} received from ${event.payload.src_country} to ${event.payload.dst_country}`,
+      );
+      break;
+    default:
+      console.log("Unrecognizable Event");
+  }
+}
+
+function eventListen(events: AppEvent[]) {
+  for (const event of events) {
+    const information = emitEvent(event.type, event.payload);
+    console.log(information);
+    statusCheck(event);
+  }
 }
